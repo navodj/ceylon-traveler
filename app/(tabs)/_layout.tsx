1039c7b0bc040c8a3bba1b32942be4de6721b86e
Chalaka-Ccb <COHNDSE242F-074@student.nibm.lk>
2025-10-11 09:42:34 +0530

    Theme & Color set

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,54 @@
+import useTheme from "../hooks/UseTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+const TabsLayout = () => {
+  const { colors } = useTheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
+        tabBarStyle: {
+          backgroundColor: colors.surface,
+          borderTopWidth: 1,
+          borderTopColor: colors.border,
+          height: 90,
+          paddingBottom: 30,
+          paddingTop: 10,
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Todos",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Trips"
+        options={{
+          title: "Trips",
+          tabBarIcon: ({ color, size }) => <Ionicons name="airplane-outline" size={size} color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => <Ionicons name="settings" size={size} color={color} />,
+        }}
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
