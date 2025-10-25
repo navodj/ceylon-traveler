e0af205ce9bbc9fddf6ab589391821b1e7d9ba7c
K K I N RANAWEERA <imaniranaweera19@gmail.com>
2025-10-25 16:07:58 +0530

    Create InputsPage component to display TravelGuideScreen

---
+++ app/auth/details.tsx
@@ -0,0 +1,10 @@
+// File: app/inputs.tsx
+
+import React from 'react';
+// This imports your form screen from the components folder
+import TravelGuideScreen from '../components/details';
+
+export default function InputsPage() {
+    // This page's only job is to show your component
+    return <TravelGuideScreen />;
+}
