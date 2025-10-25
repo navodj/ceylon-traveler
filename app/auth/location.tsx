e1a6dcf2cdd789ad3d146127f98569cc16948fe9
K K I N RANAWEERA <imaniranaweera19@gmail.com>
2025-10-25 20:17:06 +0530

    Add LocationPage component to display LocationScreen

---
+++ app/auth/location.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+// Import your component from the 'components' folder
+import LocationScreen from '../components/location';
+
+export default function LocationPage() {
+    // This page's only job is to show your component
+    return <LocationScreen />;
+}
